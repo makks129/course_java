0a7ce5a257ff4d51dfa076ae56c2497d7dd23180
makks129 <1291290@gmail.com>
2018-07-11 18:47:40 +0300

    pre lesson5

--- project0/src/lesson3/homework/Homework1.java
+++ project0/src/lesson3/homework/Homework1.java
@@ -1,5 +1,7 @@
 package lesson3.homework;
 
+import java.util.Scanner;
+
 /**
  * Bank deposit amount growth calculator.
  * <p>
@@ -16,7 +18,22 @@ public class Homework1 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter initial amount: ");
+        int depositAmount = scanner.nextInt();
+        System.out.print("Enter interest rate in %: ");
+        int interestRate = scanner.nextInt();
+        System.out.print("Enter number of years: ");
+        int years = scanner.nextInt();
+
+        for (int i = 0; i < years; i++) {
+            depositAmount += depositAmount * (interestRate * 0.01);
+        }
+
+        System.out.println("Deposit amount after " + years +
+                " years, at " + interestRate +
+                "% interest rate will be " + depositAmount);
 
     }
 
---
+++ project0/src/lesson5/Test.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+import javax.swing.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        JOptionPane.showInputDialog("Enter your name:");
+
+
+    }
+
+}
